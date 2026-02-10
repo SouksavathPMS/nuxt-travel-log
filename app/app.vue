d295e5058cf94e5f36ad072c4d23ef862622d3f2
KYE <91107096+SouksavathPMS@users.noreply.github.com>
2026-02-10 16:34:27 +0700

    Tl 11 (#52)
    * :sparkles: setup tailwind / daisy ui / icons
    
    * :sparkles: add landing page
    
    ---------
    
    Co-authored-by: SouksavathPMS <kaitk452@gmail.com>

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    <button class="btn btn-neutral">
-      Neutral <Icon name="tabler:map" width="24" height="24" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
