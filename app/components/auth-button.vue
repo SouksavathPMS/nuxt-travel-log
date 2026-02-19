ff7eeaf5085f7c79415e0d72d0d7fa130dbc5857
KYE <91107096+SouksavathPMS@users.noreply.github.com>
2026-02-19 10:43:14 +0700

    Tl 17 (#58)
    * Adding sign-out feature and its loading
    
    * Adding signout icon before the signout text
    
    ---------
    
    Co-authored-by: SouksavathPMS <kaitk452@gmail.com>

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -5,9 +5,34 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
-    Sign in with GitHub
-    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
-    <Icon v-else name="tabler:brand-github" size="22" />
-  </button>
+  <div>
+    <div v-if="authStore.user && !authStore.loading" class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn m-1">
+        <div v-if="authStore.user.image" class="avatar">
+          <div class="w-6 rounded-full">
+            <img :src="authStore.user.image">
+          </div>
+        </div>
+        {{ authStore.user.name }}
+      </div>
+      <ul tabindex="-1" class="dropdown-content menu bg-base-200 rounded-box z-1 w-32 p-2 shadow-sm">
+        <li>
+          <NuxtLink to="/dashboard">
+            Dashboard
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/sign-out">
+            <Icon name="tabler:logout-2" size="22" />
+            Sign out
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+      Sign in with GitHub
+      <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+      <Icon v-else name="tabler:brand-github" size="22" />
+    </button>
+  </div>
 </template>
