7a52322f6bf3dd86d5f2c845dc19f2e1fba2d479
SouksavathPMS <kaitk452@gmail.com>
2026-02-19 15:09:36 +0700

    navigate the app with a sidebar

---
+++ app/components/sizebar-button.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <div class="tooltip-right" :data-tip="!props.showLabel ? props.label : undefined" :class="{ tooltip: !showLabel }">
+    <NuxtLink :to="props.href" :class="{ 'bg-base-200': route.path === props.href }" class="flex justify-start gap-2 p-4 hover:bg-base-300 hover:cursor-pointer flex-nowrap">
+      <Icon :name="props.icon" size="24" />
+      <Transition name="grow">
+        <span v-if="props.showLabel">{{ props.label }}</span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.1s;
+}
+.grow-leave-active {
+  animation: grow 0.1s reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+const isSidebarOpen = ref(false);
 
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+});
+
+function toggleSizebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
 </script>
 
 <template>
-  <div>
-    <h1>You are logged in!</h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-100 transition-all duration-300 ease-in-out" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div class="flex hover:cursor-pointer hover:bg-base-200 p-4" :class="{ 'justify-end': isSidebarOpen, 'justify-center': !isSidebarOpen }">
+        <Icon :name="isSidebarOpen ? 'tabler:chevron-left' : 'tabler:chevron-right'" size="32" @click="toggleSizebar" />
+      </div>
+      <SizebarButton :show-label="isSidebarOpen" label="Locations" icon="tabler:map" href="/dashboard" />
+      <SizebarButton :show-label="isSidebarOpen" label="Add Location" icon="tabler:square-rounded-plus" href="/dashboard/add" />
+      <div class="divider" />
+      <SizebarButton :show-label="isSidebarOpen" label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+    </div>
+    <div class="flex-1">
+      <h1>Map goes here</h1>
+    </div>
   </div>
 </template>
