0ca2d3fa1c0fff72716780dc370376490d0cd2d9
SouksavathPMS <kaitk452@gmail.com>
2026-02-19 15:31:05 +0700

    Fix DB_FILE_NAME env to optional

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -14,7 +14,7 @@ dotenv.config({ path: envPath });
 
 const EnvSchema = z.object({
   NODE_ENV: z.string().default("development"),
-  DB_FILE_NAME: z.string(),
+  DB_FILE_NAME: z.string().optional(),
   TURSO_AUTH_TOKEN: z.string().optional(),
   TURSO_DATABASE_URL: z.string(),
   BETTER_AUTH_SECRET: z.string(),
@@ -30,7 +30,7 @@ tryParseEnv(EnvSchema);
 /* eslint-disable node/no-process-env */
 const parsed = EnvSchema.safeParse(process.env);
 
-const isBuildPhase = process.env.VERCEL === "1" || process.env.CI === "true" || process.env.npm_lifecycle_event === "prepare";
+const isBuildPhase = (process.env.VERCEL === "1" && !process.env.NODE_ENV) || process.env.CI === "true" || process.env.npm_lifecycle_event === "prepare";
 
 export const configEnv = (parsed.success)
   ? parsed.data
--- app/lib/try-parse-env.ts
+++ app/lib/try-parse-env.ts
@@ -20,7 +20,7 @@ export default function tryParseEnv<T extends ZodRawShape>(
       const message = `Missing required environment variables:\n${missingKeys}\nTip: If you are deploying to Vercel, ensure these are added to your Project Settings.`;
 
       // If we're in a build or prepare phase, we should not crash the process
-      const isBuildPhase = process.env.VERCEL === "1" || process.env.CI === "true" || process.env.npm_lifecycle_event === "prepare";
+      const isBuildPhase = (process.env.VERCEL === "1" && !process.env.NODE_ENV) || process.env.CI === "true" || process.env.npm_lifecycle_event === "prepare";
 
       if (isBuildPhase) {
         console.warn(`[WARNING] ${message}`);
